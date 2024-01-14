806cdc8660999818d8c367f7b3f60c4a1387c4c9
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-01-14 17:19:03 +0000

    inserted the DOM elements in logic.js

---
+++ assets/js/logic.js
@@ -0,0 +1,9 @@
+//Taking the DOM elements that are to be manipulated
+
+var questionsEl = document.querySelector("#questions"); 
+var timerEl = document.querySelector("#time"); 
+var choicesEl = document.querySelector("#choices"); 
+var submitButton = document.querySelector("#submit"); 
+var startButton = document.querySelector("#start"); 
+var initialsEl = document.querySelector("#initials"); 
+var feedbackEl = document.querySelector("#feedback"); 
