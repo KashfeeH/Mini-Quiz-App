38c0ffe73c884dde7eb07a2d590c0a8c982cebf0
KashfeeH <151862358+KashfeeH@users.noreply.github.com>
2024-01-14 16:52:06 +0000

    added the questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,101 @@
+var questions = [ 
+	{ 
+		prompt: `JavaScript is an ----------- language?`, 
+		options: [ 
+			"Object-oriented", 
+			"Object-based", 
+			"Procedural", 
+			"None of the above", 
+		], 
+		answer: "Object-oriented", 
+	}, 
+
+	{ 
+		prompt: `Which of the following keywords is used to define a variable in Javascript?`, 
+		options: [ 
+			"Let", 
+			"Var", 
+			"Const", 
+			"All of the above", 
+		], 
+		answer: "All of the above", 
+	}, 
+
+	{ 
+		prompt: `Which of the following methods is used to access HTML elements using Javascript?`, 
+		options: [ 
+			"getElementbyId()", 
+			"getElementsbyClassName()", 
+			"appendChild()", 
+			"both 1 and 2", 
+		], 
+		answer: "both 1 and 2", 
+	}, 
+
+	{ 
+		prompt: `Upon encountering empty statements, what does the Javascript Interpreter do?`, 
+		options: [
+    "Throws an error", 
+    "Ignores the statements", 
+    "Alerts a warning", 
+    "None of the above"
+  ], 
+		answer: "Ignores the statements", 
+	}, 
+
+	{ 
+		prompt: `Which of the following methods can be used to display data in some form using Javascript?`, 
+		options: [ 
+			"document.write()", 
+			"console.log()", 
+			"window.alert()", 
+			"All of the above", 
+		], 
+		answer: "All of the above", 
+	}, 
+
+  { 
+		prompt: `Which of the following is not a Javascript framework?`, 
+		options: [ 
+			"Node", 
+			"Vue", 
+			"React", 
+			"Photoshop", 
+		], 
+		answer: "Photoshop",
+	}, 
+
+  { 
+		prompt: `What keyword is used to check whether a given property is valid or not?`, 
+		options: [ 
+			"is in", 
+			"in", 
+			"exists", 
+			"lies", 
+		], 
+		answer: "in", 
+	}, 
+  
+  { 
+		prompt: `What does the ‘toLocateString()’ method do in JS??`, 
+		options: [ 
+			"Returns a localized object representation", 
+			"Returns a parsed string", 
+			"Returns a localized string representation of an object", 
+			"None of the above", 
+		], 
+		answer: "Returns a localized string representation of an object", 
+	}, 
+
+  { 
+		prompt: `Which function is used to serialize an object into a JSON string in Javascript?`, 
+		options: [ 
+			"stringify()", 
+			"parse()", 
+			"convert()", 
+			"parsify()", 
+		], 
+		answer: "stringify()", 
+	}, 
+]; 
+
